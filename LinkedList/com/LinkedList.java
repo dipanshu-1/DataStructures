bedccf70bdac85d4b261e65908ef0defa0401a56
Dipanshu 1 <Dipanshu.1@godigit.com>
2024-07-29 01:55:13 +0530

    LinkedList Implementation

---
+++ LinkedList/com/LinkedList.java
@@ -0,0 +1,82 @@
+package LinkedList.com;
+
+import java.util.Scanner;
+
+public class LinkedList {
+    Node head; // head of list
+
+    // Linked list Node.
+    // This inner class is made static
+    // so that main() can access it
+    static class Node {
+
+        int data;
+        Node next;
+
+        // Constructor
+        Node(int d)
+        {
+            data = d;
+            next = null;
+        }
+    }
+
+    // Method to insert a new node
+    public static LinkedList insert(LinkedList list, int data)
+    {
+        // Create a new node with given data
+        Node new_node = new Node(data);
+
+
+        // If the Linked List is empty,
+        // then make the new node as head
+        if (list.head == null) {
+            list.head = new_node;
+        }
+        else {
+            // Else traverse till the last node
+            // and insert the new_node there
+            Node last = list.head;
+            while (last.next != null) {
+                last = last.next;
+            }
+
+            // Insert the new_node at last node
+            last.next = new_node;
+        }
+
+        // Return the list by head
+        return list;
+    }
+    public static void printList(LinkedList list)
+    {
+        Node currNode = list.head;
+
+        System.out.print("LinkedList: ");
+
+        // Traverse through the LinkedList
+        while (currNode != null) {
+            // Print the data at current node
+            System.out.print(currNode.data + " ");
+
+            // Go to next node
+            currNode = currNode.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of Linked List");
+        int n= sc.nextInt();
+
+        System.out.println("Enter the element that you want to add in the linked list");
+
+        for(int i=0;i<n;i++)
+            list=insert(list, sc.nextInt());
+
+        System.out.println("Linked List is: ");
+        printList(list);
+    }
+}
